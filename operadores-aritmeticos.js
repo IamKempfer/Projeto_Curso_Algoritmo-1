dc1090f9a6eafeb11ce2e5648ce308221d1d8373
Victor Kempfer <victor.kempfer77@gmail.com>
2020-08-28 17:25:15 -0300

    Removido os comentarios do fonte

--- operadores-aritmeticos.js
+++ operadores-aritmeticos.js
@@ -1,14 +1,3 @@
-/*
-  resultado, valor1, valor2, valor3: real
-
-Inicio
-// Seção de Comandos, procedimento, funções, operadores, etc... 
-  valor1 := 10
-  valor2 := 10
-  valor3 := 10
-  resultado := ((valor1 + valor2+ valor2) / valor3) % 2
-  escreval("resultados: ", resultado)
-*/
 
 var   resultado, valor1, valor2, valor3;
 
